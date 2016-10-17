083b2fda4f3532c972c7c6e7e65deb426adb69ff
Augus-top <acpluschkat@hotmail.com>
2016-10-16 22:52:11 -0200

    Random.org

--- src/random/Random.java
+++ src/random/Random.java
@@ -5,10 +5,11 @@
  */
 package random;
 
-import metodos.CongruenteLinear;
-import metodos.RandomGenerator;
-import seeder.Seeder;
-import seeder.TimeMemorySeed;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.random.api.RandomOrgClient;
+import randomjsonrpc.RandomJSONRPC;
 
 /**
  *
@@ -22,9 +23,22 @@ public class Random {
     public static void main(String[] args) {
 //        RandomGenerator generator = new CongruenteLinear(5234, 4567, 1024, 123475, 10);
 //        generator.generate();
-        Seeder s = new TimeMemorySeed();
-        for (int i = 0; i < 10; i++) {
-            System.out.println(s.getSeed());
+//        Seeder s = new TimeMemorySeed();
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(s.getSeed());
+//        }
+        
+        // Limite diário de requets (1000)
+        // Código do jar: https://github.com/RandomOrg/JSON-RPC-Java
+        RandomOrgClient client = RandomOrgClient.getRandomOrgClient("d65d55e4-ac90-4e06-8bd0-1bb3d31947a6");
+        int numeroInts = 10;
+        try {
+            int n[] = client.generateIntegers(numeroInts, 0, 500);
+            for (int i = 0; i < numeroInts; i++) {
+                System.out.println(n[i]);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(RandomJSONRPC.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
